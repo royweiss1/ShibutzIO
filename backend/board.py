6ec7a3708a21093ae0510bef5f79277e82497a45
royweiss <weissroy@post.bgu.ac.il>
2023-06-01 11:24:24 +0300

    init

---
+++ backend/board.py
@@ -0,0 +1,12 @@
+import player
+
+DEFAULT_LOCATION = {2: [], 4: []}
+
+
+class Board:
+    def __init__(self, size, numPlayers):
+        self.SIZE = size
+        self.numOfPlayers = numPlayers
+        self.players = []  # to init
+        for i in DEFAULT_LOCATION[numPlayers]:
+            self.players.append(player.Player(i))
---
+++ backend/player.py
@@ -0,0 +1,9 @@
+class Player:
+    def __init__(self, position):
+        self.position = position #pair x,y
+        self.area = [] #list of coordinats of area
+        self.halfCaptured = [] # by order of capture
+        self.keys = 0 #num of keys
+        self.keysPositions = [] #
+
+
